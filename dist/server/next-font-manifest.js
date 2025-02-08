c1bb067cd3b5f05b64ee1d7d8b353870b54e765a
EnzoVesseron <zirewell@gmail.com>
2025-02-08 23:39:11 +0100

    remove dist

--- dist/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/projects/sb1-57yzz2xx/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
